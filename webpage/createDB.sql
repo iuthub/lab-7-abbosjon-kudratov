ecb9e71bd9b3819d0ea98703e2b81b44a7a55445
abbosjon-kudratov <abbosjon.kudtratov@gmail.com>
2019-03-16 18:28:35 -0400

    blog: databases created :)

---
+++ webpage/createDB.sql
@@ -0,0 +1,20 @@
+ create table users
+ 	(	id int AUTO_INCREMENT,
+ 		username varchar(250),
+ 		email varchar(250),
+ 		password varchar(250),
+ 		fullname varchar(250),
+ 		dob date,
+ 		primary key (id),
+ 		 );
+
+
+ create table posts
+ 	(	id int AUTO_INCREMENT,
+ 		title varchar(250),
+ 		body text,
+ 		publishDate date,
+ 		userID int,
+ 		primary key (id),
+ 		foreign key (userID) REFERENCES users(id)
+ 		 );
